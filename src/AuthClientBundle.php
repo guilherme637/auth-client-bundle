611882c86ab078a6147f88ea25c4d8db540180a5
gui_chaves37 <guilhermejr296@icloud.com>
2023-11-01 22:55:21 -0300

    Corrigindo file Bundle

--- src/AuthClientBundle.php
+++ src/AuthClientBundle.php
@@ -2,9 +2,14 @@
 
 namespace Zuske\AuthClient;
 
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Zuske\AuthClient\DependencyInjection\AuthClientExtension;
 
 class AuthClientBundle extends Bundle
 {
-
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        return new AuthClientExtension();
+    }
 }
--- teste.php
+++ teste.php
@@ -1,1 +1,36 @@
 <?php
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$configs['zuske_auth_client']  = ['auth'=> [
+    'resource_owner' => null,
+    'client_id' => null,
+    'client_secret' => null,
+    'host_client' => null,
+    'redirect_uris' => null,
+    'response_type' => null,
+    'grant_type' => null,
+    'scope' => null,
+]];
+
+// Build the service container
+$container = new ContainerBuilder();
+$extension = new \Zuske\AuthClient\DependencyInjection\AuthClientExtension();
+$extension->load($configs, $container);
+$container->compile();
+var_dump($container);
+exit();
+// Set and create the output directory
+$outDir = __DIR__ . '/out';
+if (!file_exists($outDir))
+    mkdir($outDir);
+// Define the file name and contents
+$index = count(scandir($outDir));
+$path = "$outDir/document$index.docx";
+$text = 'Hello world!';
+// Use the services in the bundle
+$id = $container->get('app.document.db')->newDocument($text);
+$container->get('app.document.formatter')->format($id, $path);
+echo "The document is at '$path'\n";
